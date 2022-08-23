d5a75c3f16d5f1f080f3e9a247638a5e6125a9b7
SachinKumar0381 <sachinkuom0591998@gmail.com>
2022-08-23 21:06:29 +0530

    files are added

---
+++ freshly/src/Redux/store.js
@@ -0,0 +1,10 @@
+import {combineReducers, legacy_createStore,applyMiddleware,compose} from 'redux';
+import thunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const rootReducer = combineReducers({
+})
+const store=legacy_createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)));
+
+export {store};
