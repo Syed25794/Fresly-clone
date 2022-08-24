9c5390d89759308e497b3f703537676d77e5fd63
SachinKumar0381 <sachinkuom0591998@gmail.com>
2022-08-24 22:08:34 +0530

    plan page added

--- freshly/src/index.js
+++ freshly/src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./Redux/store";
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
+    <ChakraProvider>
     <App />
-    </Provider>
+    </ChakraProvider>
+    // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
