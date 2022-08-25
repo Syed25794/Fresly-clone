195dcd43ef9e8c2e1e5b24a5b1b310c417374568
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2022-08-25 12:50:17 +0530

    login basic ui done

--- freshly/src/App.js
+++ freshly/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
-
+import Login from "./Pages/Authentication/Login";
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+      <Login />
     </div>
   );
 }
 
 export default App;
-
--- freshly/src/Redux/store.js
+++ freshly/src/Redux/store.js
@@ -1,10 +1,18 @@
-// import {combineReducers, legacy_createStore,applyMiddleware,compose} from 'redux';
-// import thunk from "redux-thunk";
+import {
+  combineReducers,
+  legacy_createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
 
-// // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ || compose();
 
-// // const rootReducer = combineReducers({
-// // })
-// // const store=legacy_createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)));
+const rootReducer = combineReducers({});
+const store = legacy_createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
-// // export {store};
+export { store };
