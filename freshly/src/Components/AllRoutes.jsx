d7efb22df70bfcc653f75b330c1d7ed327ae84ad
Sagar Bhuva <sagarbhuva43@gmail.com>
2022-08-25 11:03:24 +0530

    adede all route

--- freshly/src/Components/AllRoutes.jsx
+++ freshly/src/Components/AllRoutes.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../Pages/Authentication/Login";
+import SignUp from "../Pages/Authentication/SignUp";
+import Cart from "../Pages/Cart/Cart";
+import Checkout from "../Pages/Checkout/Checkout";
+import DateDeliver from "../Pages/DateDelivery/DateDeliver";
+import Plans from "../Pages/Plans/Plans"
+import HomePage from "./HomePage/HomePage";
 
-function AllRoutes() {
+
+
+const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/cart" element={<Cart/>} />
+      <Route path="/deliverydate" element={<DateDeliver/>} />
+      <Route path="/plans" element={<Plans/>} />
+      {/* <Route path="/payment" element={<Payment />} />
+      <Route path="/thankyou" element={<Thankyou />} /> */}
+      {/* <Route path="/plansandmenus" element={<MenuPlans />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/checkout" element={<Checkout/>} />
+    </Routes>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
