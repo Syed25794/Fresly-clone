6969d7557971e8b2c2aed10e968c1049fc95fcd0
Vrushabh Veer <veervrushabh@gmail.com>
2022-08-26 21:41:03 +0530

    added checkout file

--- freshly/src/Components/AllRoutes.jsx
+++ freshly/src/Components/AllRoutes.jsx
@@ -5,6 +5,7 @@ import SignUp from "../Pages/Authentication/SignUp";
 import Cart from "../Pages/Cart/Cart";
 import Checkout from "../Pages/Checkout/Checkout";
 import DateDeliver from "../Pages/DateDelivery/DateDeliver";
+import Help from "../Pages/Help";
 import HomePage from "../Pages/HomePage/HomePage";
 import Plans from "../Pages/Plans/Plans.jsx"
 
@@ -22,6 +23,7 @@ const AllRoutes = () => {
       <Route path="/thankyou" element={<Thankyou />} /> */}
       {/* <Route path="/plansandmenus" element={<MenuPlans />} /> */}
       <Route path="/login" element={<Login />} />
+      <Route path="/help" element={<Help />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/checkout" element={<Checkout/>} />
     </Routes>
